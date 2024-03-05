24d57fa2edd7167066867d2b2faaa064bfd2e5a0
Oyisile <160606684+Oyisile@users.noreply.github.com>
2024-03-05 17:35:01 +0200

    Added the search functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+function refreshWeatherTemp(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    
+}
+
+function searchCity(city) {
+let apiKey = "9904aa93dbdo0391tcb96280730feb23";
+let apiUrl =
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherTemp);
+}
+
 function searchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
+
+searchCity(searchInput.value);
+
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Paris")
